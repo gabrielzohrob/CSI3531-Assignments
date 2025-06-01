4829b97628421c0e6101d16955a0cce6fcdd74bd
pierealhaddad <151982098+pierealhaddad@users.noreply.github.com>
2025-06-01 17:30:54 -0400

    pushed code

--- assignment 1/cpr.c
+++ assignment 1/cpr.c
@@ -12,11 +12,21 @@ Description: Ce programme contient le code pour la creation
 Explication du processus zombie
 (point 5 de "A completer" dans le devoir):
 
-	(s.v.p. completez cette partie);
+	Quand un processus termine et que tous ses 
+	ressources ne sont pas encore relachées, il est 
+	en dans l'état "Zombie".
 
+	Dans notre programme, quand l'enfant termine,
+	il envoie un signal au parent, mais comme le
+	parent n'utilise pas wait(), les informations
+	de l'enfantne sont pas récupérées. Pendant les 
+	10 secondes de sleep(), on peut voir l'enfant
+	en état zombie.
 -------------------------------------------------------------*/
 #include <stdio.h>
 #include <sys/select.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /* Prototype */
 void creerEnfantEtLire(int );
@@ -64,7 +74,82 @@ Description:
 
 void creerEnfantEtLire(int prcNum)
 {
+	//CAS DE BASE: Nous avons un seul processus ie. processus 1
+    if(prcNum == 1)
+	{
+		fprintf (stderr, "Processus 1 commence\n");
+		sleep(5);
+		fprintf (stderr, "Processus 1 termine\n");
+		sleep(10);
+		return;
+	}
 
-    /* S.V.P. completez cette fonction selon les
-       instructions du devoirs. */
+	//printf("Processus %d commence\n", prcNum);
+	int pipes[2], tuy, pid;
+	tuy = pipe(pipes);
+
+	//error handling pipe
+	if (tuy == -1)
+	{
+		fprintf(stderr,"Erreur dans la creation du tuyau\n");
+		exit(-1);
+	}
+
+	pid = fork();
+	//error handling fork
+	if (pid < 0)
+	{
+		fprintf (stderr, "Erreur dans la creation du processus enfant\n");
+		exit(-1);
+	}
+
+	//child process
+	else if (pid == 0)
+	{
+		// close the read
+		close(pipes[0]);
+		
+		//Redirect stdout to write end of the pipe
+		int dp2= dup2(pipes[1], 1);
+		if (dp2 == -1)
+		{
+			fprintf(stderr, "Erreur lors de la dup");
+			exit(-1);
+		}
+
+		//close the write-end 
+		close(pipes[1]);
+
+		//re-executing the program
+		int newPRC = prcNum-1;
+        execvp("cpr.c", "cpr" "newPRC");
+		fprintf(stderr, "Erreur lors de l'éxecution de exec.\n");
+		exit(-1);
+	}
+
+	//parent process
+	else
+	{
+		//close the write-end of the pipe
+		close(pipes[1]);
+
+		//parent proccess starts
+		fprintf(stderr, "Processus %d commence\n", prcNum);
+
+		//read from the buffer
+		char buffer[1024];
+        ssize_t n;
+        while ((n = read(pipes[0], buffer, sizeof(buffer))) > 0)
+        {
+            write(1, buffer, n); // Print to own stdout
+        }
+
+		//closing the read end
+		close(pipes[0]);
+
+		// goes to the terminal if original process is reached, goes into the pipe if it is someone else's child
+		fprintf(stderr, "Processus %d termine\n", prcNum);
+
+		sleep(10);
+	}
 }
